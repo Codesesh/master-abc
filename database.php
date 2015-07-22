523d593ed1dad299d7e5e8dc295e4f9816c428d3
ifusionnz1 <ifusionnz@gmail.com>
2015-07-22 22:50:40 +1200

    changing db class

--- database.php
+++ database.php
@@ -1,12 +1,25 @@
 <?php
 
-
+private static $has_many = array();
 
 /**
 * 
 */
 class Database extends Page
 {
+	$this->database = database;
 	
+}
+
+
+/**
+* 
+*/
+class Another extends page
+{
 	
+	public function hello(hello)
+	{
+		echo 'hello';
+	}
 }
